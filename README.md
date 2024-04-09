878c0d3373ad33311e1c700f2cd05e628900385e
Mauro Marcosano <mauromarcosano@gmail.com>
2024-04-09 18:02:00 -0300

    Update: Grid now supports values up to 100, but not for values less than 16

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch-project
+
+In this project, I’ll be creating a pretty neat toy to flex my DOM manipulation skills. I am going to build a browser version of something between a sketchpad and an Etch-A-Sketch.
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('.board');
+const gridSize = 15;
 
-for (let i=0; i<17; i++){
-const square = document.createElement('div');
-square.classList.add('square',`-${i}`);
-container.append(square);
+for (let i=0; i<(gridSize); i++){
+  const row = document.createElement('div');
+  row.classList.add('row',`-${i}`);
+  container.appendChild(row);
+
+  for(let j=0; j<gridSize;j++){
+    const square = document.createElement('div');
+    square.classList.add('square',`-${i}-${j}`);
+    row.appendChild(square);
+  }
 }
